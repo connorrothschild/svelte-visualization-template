7c197b68e21031cedc36b1674e42de3aa7fd0dfa
Connor Rothschild <connorrothschild@gmail.com>
2022-10-16 15:16:19 -0500

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  resolve: {
+    alias: {
+      "$lib": path.resolve('./src/lib'),
+      "$components": path.resolve("./src/components"),
+      "$data": path.resolve("./src/data"),
+      "$routes": path.resolve("./src/routes"),
+    }
+  }
 })
