16831371633d45462d30b23a221a814ac7642a9e
Connor Rothschild <connorrothschild@gmail.com>
2022-10-16 13:40:49 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,59 @@
+<script>
+</script>
+
+<main>
+  <h1>Let's make a chart 😎</h1>
+  <h2>
+    Get started by deleting all of the contents in <pre>App.svelte</pre>
+    🗑
+  </h2>
+  <footer>
+    For help, <a
+      href="https://twitter.com/CL_Rothschild"
+      target="_blank"
+      rel="noopener noreferrer">DM Connor on Twitter ✉️</a
+    >
+  </footer>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background: #f0f0f0;
+  }
+
+  h1 {
+    font-size: 3rem;
+    margin-bottom: 1rem;
+    font-weight: 700;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    color: #333;
+    margin-bottom: 2rem;
+  }
+
+  footer {
+    font-size: 1rem;
+    color: #333;
+  }
+  pre {
+    padding: 3px;
+    display: inline;
+    margin: 0;
+    background: #f3f3f3;
+    border-radius: 3px;
+  }
+
+  a {
+    color: #ff3e00;
+    text-decoration: inherit;
+  }
+</style>
